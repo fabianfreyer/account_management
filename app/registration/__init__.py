bf225b09ed5a4cbcce18552dee5331538a57a56a
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2017-03-02 23:37:21 +0100

    Add functionality to log in using uni token

--- app/registration/__init__.py
+++ app/registration/__init__.py
@@ -1,8 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, g
+from flask_httpauth import HTTPTokenAuth
 
+token_auth = HTTPTokenAuth(scheme='ZaPF-Token')
 registration_blueprint = Blueprint('registration', __name__, template_folder='templates/')
 
 from . import models, views
 
+@token_auth.verify_token
+def verify_token(token):
+    uni = models.Uni.query.filter_by(token=token).first()
+    g.uni = uni
+    return uni is not None
+
 def init_app(app):
-    pass
+    return app
